4ac63badf8e69f33ed31d9986ab775d7b8c9cd60
Ben Milner <bmilner88@gmail.com>
2023-04-13 13:01:01 -0700

    Added custom size and icon

--- public/electron.js
+++ public/electron.js
@@ -7,18 +7,14 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 function createWindow() {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1400,
+    height: 800,
+    icon: __dirname + "/favicon.ico",
     webPreferences: { nodeIntegration: true, contextIsolation: false },
   });
-  // and load the index.html of the app.
   console.log(__dirname);
   mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
