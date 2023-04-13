ab3a55a2f898daec136acfc6f2312db10940155f
Ben Milner <bmilner88@gmail.com>
2023-04-13 15:44:11 -0700

    Update App.jsx

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,141 +1,57 @@
-import Versions from './components/Versions'
-import icons from './assets/icons.svg'
+import { useState } from 'react'
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
+//import icons from './assets/icons.svg'
 
 function App() {
+  const [videoSrc, setVideoSrc] = useState('')
+  const [imageFile, setImageFile] = useState({})
+  const [soundFile, setSoundFile] = useState({})
+
+  const ffmpeg = createFFmpeg({
+    log: true
+  })
+
+  const handleChangeImage = (e) => {
+    setImageFile(e.target.files[0])
+  }
+  const handleChangeSound = (e) => {
+    setSoundFile(e.target.files[0])
+  }
+
+  const createVideo = async () => {
+    await ffmpeg.load()
+    ffmpeg.FS('writeFile', 'image.png', await fetchFile(imageFile))
+    ffmpeg.FS('writeFile', 'sound.mp3', await fetchFile(soundFile))
+    await ffmpeg.run(
+      '-framerate',
+      '1/10',
+      '-i',
+      'image.png',
+      '-i',
+      'sound.mp3',
+      '-c:v',
+      'libx264',
+      '-t',
+      '10',
+      '-pix_fmt',
+      'yuv420p',
+      '-vf',
+      'scale=1920:1080',
+      'test.mp4'
+    )
+    const data = ffmpeg.FS('readFile', 'test.mp4')
+    setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })))
+  }
+
   return (
     <div className="container">
-      <Versions></Versions>
-
-      <svg className="hero-logo" viewBox="0 0 900 300">
-        <use xlinkHref={`${icons}#electron`} />
-      </svg>
-      <h2 className="hero-text">You{"'"}ve successfully created an Electron project with React</h2>
-      <p className="hero-tagline">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-
-      <div className="links">
-        <div className="link-item">
-          <a target="_blank" href="https://evite.netlify.app" rel="noopener noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="link-item link-dot">•</div>
-        <div className="link-item">
-          <a
-            target="_blank"
-            href="https://github.com/alex8088/electron-vite"
-            rel="noopener noreferrer"
-          >
-            Getting Help
-          </a>
-        </div>
-        <div className="link-item link-dot">•</div>
-        <div className="link-item">
-          <a
-            target="_blank"
-            href="https://github.com/alex8088/quick-start/tree/master/packages/create-electron"
-            rel="noopener noreferrer"
-          >
-            create-electron
-          </a>
-        </div>
-      </div>
-
-      <div className="features">
-        <div className="feature-item">
-          <article>
-            <h2 className="title">Configuring</h2>
-            <p className="detail">
-              Config with <span>electron.vite.config.ts</span> and refer to the{' '}
-              <a target="_blank" href="https://evite.netlify.app/config/" rel="noopener noreferrer">
-                config guide
-              </a>
-              .
-            </p>
-          </article>
-        </div>
-        <div className="feature-item">
-          <article>
-            <h2 className="title">HMR</h2>
-            <p className="detail">
-              Edit <span>src/renderer</span> files to test HMR. See{' '}
-              <a
-                target="_blank"
-                href="https://evite.netlify.app/guide/hmr-in-renderer.html"
-                rel="noopener noreferrer"
-              >
-                docs
-              </a>
-              .
-            </p>
-          </article>
-        </div>
-        <div className="feature-item">
-          <article>
-            <h2 className="title">Hot Reloading</h2>
-            <p className="detail">
-              Run{' '}
-              <span>
-                {"'"}electron-vite dev --watch{"'"}
-              </span>{' '}
-              to enable. See{' '}
-              <a
-                target="_blank"
-                href="https://evite.netlify.app/guide/hot-reloading.html"
-                rel="noopener noreferrer"
-              >
-                docs
-              </a>
-              .
-            </p>
-          </article>
-        </div>
-        <div className="feature-item">
-          <article>
-            <h2 className="title">Debugging</h2>
-            <p className="detail">
-              Check out <span>.vscode/launch.json</span>. See{' '}
-              <a
-                target="_blank"
-                href="https://evite.netlify.app/guide/debugging.html"
-                rel="noopener noreferrer"
-              >
-                docs
-              </a>
-              .
-            </p>
-          </article>
-        </div>
-        <div className="feature-item">
-          <article>
-            <h2 className="title">Source Code Protection</h2>
-            <p className="detail">
-              Supported via built-in plugin <span>bytecodePlugin</span>. See{' '}
-              <a
-                target="_blank"
-                href="https://evite.netlify.app/guide/source-code-protection.html"
-                rel="noopener noreferrer"
-              >
-                docs
-              </a>
-              .
-            </p>
-          </article>
-        </div>
-        <div className="feature-item">
-          <article>
-            <h2 className="title">Packaging</h2>
-            <p className="detail">
-              Use{' '}
-              <a target="_blank" href="https://www.electron.build" rel="noopener noreferrer">
-                electron-builder
-              </a>{' '}
-              and pre-configured to pack your app.
-            </p>
-          </article>
-        </div>
-      </div>
+      <video src={videoSrc} controls></video>
+      <br />
+      <input type="file" id="image" accept="image/*" onChange={handleChangeImage}></input>
+      <br />
+      <input type="file" id="sound" accept="sound/*" onChange={handleChangeSound}></input>
+      <br />
+      <button onClick={createVideo}>Create a video from files above</button>
     </div>
   )
 }
