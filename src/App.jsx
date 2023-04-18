1895401691ffd05349285263c303aff4b090678c
Ben Milner <bmilner88@gmail.com>
2023-04-17 22:04:10 -0700

    Test project working

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import "./App.css";
 
 function App() {
+  const [videoSrc, setVideoSrc] = useState("");
+
+  const [imageFile, setImageFile] = useState({});
+  const [soundFile, setSoundFile] = useState({});
+
+  const ffmpeg = createFFmpeg({
+    log: true,
+  });
+
+  const handleChangeImage = (e) => {
+    setImageFile(e.target.files[0]);
+  };
+
+  const handleChangeSound = (e) => {
+    setSoundFile(e.target.files[0]);
+  };
+
+  const createVideo = async () => {
+    await ffmpeg.load();
+
+    ffmpeg.FS("writeFile", "image.png", await fetchFile(imageFile));
+    ffmpeg.FS("writeFile", "sound.mp3", await fetchFile(soundFile));
+
+    await ffmpeg.run(
+      "-framerate",
+      "1/10",
+      "-i",
+      "image.png",
+      "-i",
+      "sound.mp3",
+      "-c:v",
+      "libx264",
+      "-t",
+      "10",
+      "-pix_fmt",
+      "yuv420p",
+      "-vf",
+      "scale=1920:1080",
+      "output.mp4"
+    );
+
+    const data = ffmpeg.FS('readFile', 'output.mp4');
+
+    setVideoSrc(URL.createObjectURL(new Blob([data.buffer], {type: 'video/mp4'})));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video src={videoSrc} controls></video>
+      <br />
+      <input
+        type="file"
+        id="image"
+        accept="image/*"
+        onChange={handleChangeImage}
+      ></input>
+      <br />
+      <input
+        type="file"
+        id="sound"
+        accept="sound/*"
+        onChange={handleChangeSound}
+      ></input>
+      <br />
+      <button onClick={createVideo}>Create a video from the files above</button>
     </div>
   );
 }
