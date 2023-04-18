3681c972d7f5fe92fe3a64585c769eea86104092
Ben Milner <bmilner88@gmail.com>
2023-04-17 22:54:03 -0700

    Having issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,42 @@ import "./App.css";
 function App() {
   const [videoSrc, setVideoSrc] = useState("");
 
-  const [imageFile, setImageFile] = useState({});
-  const [soundFile, setSoundFile] = useState({});
+  const [videoFile, setVideoFile] = useState({});
 
   const ffmpeg = createFFmpeg({
     log: true,
   });
 
   const handleChangeImage = (e) => {
-    setImageFile(e.target.files[0]);
-  };
-
-  const handleChangeSound = (e) => {
-    setSoundFile(e.target.files[0]);
+    setVideoFile(e.target.files[0]);
+    console.log(e.target.files[0]);
   };
 
   const createVideo = async () => {
     await ffmpeg.load();
 
-    ffmpeg.FS("writeFile", "image.png", await fetchFile(imageFile));
-    ffmpeg.FS("writeFile", "sound.mp3", await fetchFile(soundFile));
+    ffmpeg.FS("writeFile", `${videoFile.name.slice(0, -4)}.mp4`, await fetchFile(videoFile));
 
     await ffmpeg.run(
-      "-framerate",
-      "1/10",
       "-i",
-      "image.png",
-      "-i",
-      "sound.mp3",
-      "-c:v",
-      "libx264",
-      "-t",
-      "10",
-      "-pix_fmt",
-      "yuv420p",
-      "-vf",
-      "scale=1920:1080",
+      `${videoFile.name.slice(0, -4)}.mp4`,
+      "-s",
+      "1920x1080",
       "output.mp4"
     );
 
-    const data = ffmpeg.FS('readFile', 'output.mp4');
+    ffmpeg.setProgress(({ ratio }) => {
+      console.log(ratio);
+      /*
+       * ratio is a float number between 0 to 1.
+       */
+    });
 
-    setVideoSrc(URL.createObjectURL(new Blob([data.buffer], {type: 'video/mp4'})));
+    const data = ffmpeg.FS("readFile", "output.mp4");
+
+    setVideoSrc(
+      URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" }))
+    );
   };
 
   return (
@@ -55,19 +49,12 @@ function App() {
       <br />
       <input
         type="file"
-        id="image"
-        accept="image/*"
+        id="video"
+        accept="video/*"
         onChange={handleChangeImage}
       ></input>
       <br />
-      <input
-        type="file"
-        id="sound"
-        accept="sound/*"
-        onChange={handleChangeSound}
-      ></input>
-      <br />
-      <button onClick={createVideo}>Create a video from the files above</button>
+      <button onClick={createVideo}>Convert to mp4</button>
     </div>
   );
 }
